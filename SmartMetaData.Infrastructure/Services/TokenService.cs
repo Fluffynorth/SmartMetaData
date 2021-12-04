7c54b9d8b266bcb35df3387b33fa53032b8f75a8
darkcodi <trooper982@gmail.com>
2021-12-04 13:54:29 +0200

    Add GetTokenUri endpoint

--- SmartMetaData.Infrastructure/Services/TokenService.cs
+++ SmartMetaData.Infrastructure/Services/TokenService.cs
@@ -1,10 +1,12 @@
 using System.Numerics;
 using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Options;
+using Nethereum.Contracts;
 using Nethereum.JsonRpc.Client;
 using Nethereum.Web3;
 using SmartMetaData.Domain.Models.Enums;
 using SmartMetaData.Domain.Models.ValueObjects;
+using SmartMetaData.Infrastructure.Extensions;
 using SmartMetaData.Infrastructure.Models.Functions;
 using SmartMetaData.Infrastructure.Options;
 
@@ -21,17 +23,23 @@ public class TokenService : ITokenService
 
     public async Task<Result<Uri>> GetTokenUri(Address contractAddress, BigInteger tokenId, EthereumNetwork network)
     {
-        var tokenUriMessage = new Erc721TokenUriFunction
-        {
-            TokenId = tokenId,
-        };
-
         var rpcUrl = _rpcOptions.GetRpcUrl(network);
         var rpcClient = new RpcClient(rpcUrl);
         var web3 = new Web3(rpcClient);
-        var tokenUriHandler = web3.Eth.GetContractQueryHandler<Erc721TokenUriFunction>();
-        var tokenUri = await tokenUriHandler.QueryAsync<string>(contractAddress, tokenUriMessage);
 
+        var erc721Function = new Erc721TokenUriFunction { TokenId = tokenId };
+        var erc1155Function = new Erc1155UriFunction { Id = tokenId };
+
+        var erc721Task = web3.SafeQuery(contractAddress, erc721Function);
+        var erc1155Task = web3.SafeQuery(contractAddress, erc1155Function);
+
+        await Task.WhenAll(erc721Task, erc1155Task);
+
+        var values = new[] { erc721Task.Result, erc1155Task.Result };
+        if (values.All(x => x.IsFailure))
+            return Result.Failure<Uri>("Contract calls failed");
+
+        var tokenUri = values.First(x => x.IsSuccess).Value;
         if (string.IsNullOrEmpty(tokenUri))
             return Result.Failure<Uri>("TokenUri is null or empty");
 
