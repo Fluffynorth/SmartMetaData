6df816b57b0d530064f2570e860ddeffefa291a9
darkcodi <trooper982@gmail.com>
2021-12-23 02:34:35 +0200

    Fix BigIntegerConverter

--- src/api/SmartMetaData/Converters/BigIntegerConverter.cs
+++ src/api/SmartMetaData/Converters/BigIntegerConverter.cs
@@ -9,11 +9,19 @@ public class BigIntegerConverter : JsonConverter<BigInteger>
 {
     public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType != JsonTokenType.Number)
-            throw new JsonException($"Found token {reader.TokenType} but expected token {JsonTokenType.Number}");
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            using var doc = JsonDocument.ParseValue(ref reader);
+            return BigInteger.Parse(doc.RootElement.GetRawText(), NumberFormatInfo.InvariantInfo);
+        }
 
-        using var doc = JsonDocument.ParseValue(ref reader);
-        return BigInteger.Parse(doc.RootElement.GetRawText(), NumberFormatInfo.InvariantInfo);
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            using var doc = JsonDocument.ParseValue(ref reader);
+            return BigInteger.Parse(doc.RootElement.GetRawText(), NumberFormatInfo.InvariantInfo);
+        }
+
+        throw new JsonException($"Found token {reader.TokenType} but expected token {JsonTokenType.Number} or {JsonTokenType.String}");
     }
 
     public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
